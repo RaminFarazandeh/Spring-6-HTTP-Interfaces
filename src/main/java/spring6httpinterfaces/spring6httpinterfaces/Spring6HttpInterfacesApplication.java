98b4dd551c9f3871b4515c64b84b0cca392fb65a
Ramin Farazandeh <ramin.farazandeh@cas.de>
2022-05-24 14:08:02 +0200

    init

---
+++ src/main/java/spring6httpinterfaces/spring6httpinterfaces/Spring6HttpInterfacesApplication.java
@@ -0,0 +1,13 @@
+package spring6httpinterfaces.spring6httpinterfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6HttpInterfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6HttpInterfacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring6httpinterfaces/spring6httpinterfaces/Spring6HttpInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package spring6httpinterfaces.spring6httpinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6HttpInterfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
